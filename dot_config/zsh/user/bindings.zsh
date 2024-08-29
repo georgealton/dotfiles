2151a7f661d4eddce38fe1c96cc15257735d9234
George Alton <galton@evertz.com>
2024-08-29 11:20:42 +0100

    removes river

--- dot_config/zsh/user/bindings.zsh
+++ dot_config/zsh/user/bindings.zsh
@@ -1,4 +1,9 @@
 # vim like shell
 bindkey -v
-source /usr/share/fzf/key-bindings.zsh
-command -v brew && source /opt/homebrew/Cellar/fzf/$(fzf --version | cut -d' ' -f1)/shell/key-bindings.zsh
+
+for location in "/usr/share/fzf/key-bindings.zsh" "/usr/share/fzf/shell/key-bindings.zsh" "/opt/homebrew/Cellar/fzf/$(fzf --version | cut -d' ' -f1)/shell/key-bindings.zsh"; do
+    if [[ -f "$location" ]]; then
+        source "$location"
+        break
+    fi
+done
