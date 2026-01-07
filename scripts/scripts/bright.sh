d9cbc06a2ef5459a1a0ca11a4cfabcebb61ea0b3
George Alton <galton@evertz.com>
2026-01-07 10:40:52 +0000

    wrap in a function

--- scripts/scripts/bright.sh
+++ scripts/scripts/bright.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-brightness_as_percent=$(brightnessctl get --percentage --machine-readable)
-notify-send \
-    --transient \
-    --app-name 'brightnessctl' \
-    --hint "int:value:$brightness_as_percent" \
-    "Brightness: ${brightness_as_percent}%"
+main() {
+    local brightness_as_percent
+
+    brightness_as_percent=$(brightnessctl get --percentage --machine-readable)
+    notify-send \
+        --transient \
+        --app-name 'brightnessctl' \
+        --hint "int:value:$brightness_as_percent" \
+        "Brightness: ${brightness_as_percent}%"
+}
+
+main "@"
